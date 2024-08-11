a3eb8f8468070341a3a75bfdcbbaefdb0c2076b2
Kelvin Chan <50619735+khchan123@users.noreply.github.com>
2024-08-11 23:30:40 +0800

    Bump ComfyUI version to v0.0.6

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ configure() {
     APP_NAME="comfyui"
 
     # Git reference of ComfyUI (should be a commit id instead of a branch name for production)
-    COMFYUI_GIT_REF="v0.0.1"
+    COMFYUI_GIT_REF="v0.0.6"
 
     # S3 bucket for deployment files (model artifact and Lambda package)
     # Note: Adjust ComfyUIModelExecutionRole in template.yaml to grant S3 related permissions if the bucket name does not contain "SageMaker", "Sagemaker" or "sagemaker".
